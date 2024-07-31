4bdb38d2b5e7f16c254d77e947cba29789891438
Andreas Backman <anba2205@student.miun.se>
2024-08-01 00:34:00 +0200

    Setup2

---
+++ js/atlas.js
@@ -0,0 +1,46 @@
+export class Atlas {
+	/**
+	 * The data source used for this Atlas instance.
+	 */
+	#dataSource;
+
+	/**
+	 * Create a new Atlas instance with the specified data source for its data.
+	 * @param dataSource the data source to be used
+	 */
+	constructor(dataSource) {
+		this.#dataSource = dataSource;
+	}
+
+	
+	async getPets() {
+		return this.#dataSource.getPets();
+	}
+
+	
+	async getPet(petName) {
+		return this.#dataSource.getPets(petName);
+	}
+
+	
+	async getOwners() {
+		return this.#dataSource.getOwners();
+	}
+
+	
+	async getOwnersPets(ownerSsn) {
+		return this.#dataSource.getOwnersPets(ownerSsn);
+	}
+
+	async addPet(petName, species, breed, birthdate, healthstatus, ownerSsn) {
+		return this.#dataSource.addMyCourse(petName, species, breed, birthdate, healthstatus, ownerSsn);
+	}
+
+	async deletePet(petName) {
+		return this.#dataSource.deletePet(petName);
+	}
+
+	updatePetHealthStatus(petName, healthstatus) {
+		return this.#dataSource.updateMyCourse(petName, healthstatus);
+	}
+}
